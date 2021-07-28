558c62d1fc03edabcf39ab0933e14010dbade6c0
kalimeraa <hi.mrttpz@gmail.com>
2021-07-28 16:11:13 +0300

    fix e2e test for given empty case

--- frontend/tests/e2e/specs/todo_spec.js
+++ frontend/tests/e2e/specs/todo_spec.js
@@ -1,7 +1,23 @@
 describe("Todo App", () => {
+
     it("There should be an error when I clicked button with empty text", () => {
+        cy.server();
+
+        cy.route(
+            {
+                method:"POST",
+                url:"api/todos/create",
+                response: {"errors":[{"value":"","msg":"todo is required.","param":"todo","location":"body"}]},
+                status:400
+            }
+        ).as("todoRequired");
+
         cy.visit("/");
-        cy.get("button").click();
+
+        cy.get("button#addTodo").click();
+
+        cy.wait("@todoRequired");
+
         cy.get("h3#error", {timeout: 20000}).should(
             "have.text",
             "todo is required."
@@ -25,9 +41,7 @@ describe("Todo App", () => {
             .type("buy some milk")
             .should("have.value", "buy some milk");
 
-        cy.get('button').click();
-
-        cy.wait("@createTodo");
+        cy.get('button').click().wait(["@createTodo"]);
 
         cy.get("ul.list-group", {timeout: 20000}).contains(
             "li.todo-list",
